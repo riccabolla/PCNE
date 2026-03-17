f23b58ce43925c83670cd5ccf6a92c77b220b65e
Riccardo <bollach@virgilio.it>
2026-03-17 14:35:18 +0100

    update conda req

--- conda/build.sh
+++ conda/build.sh
@@ -4,5 +4,6 @@ set -u
 mkdir -p $PREFIX/bin
 install -m 755 bin/pcne $PREFIX/bin/
 install -m 755 bin/pcne_summary $PREFIX/bin/
+install -m 755 bin/pcne_long $PREFIX/bin/
 install -m 644 bin/PCNE_analysis.R $PREFIX/bin/
 install -m 644 bin/PCNE_summary.R $PREFIX/bin/
